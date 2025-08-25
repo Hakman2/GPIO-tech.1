c935d5940ecc1c60b093d8305d0656294f44d687
Hakkman <jakh3172@gmail.com>
2025-08-25 12:47:13 +0000

    fxn

--- week2/fxn.py
+++ week2/fxn.py
@@ -1,26 +1,52 @@
 #function: a block of reusable code to perform a particular tasks
-import time
-import math
-def cal_speed(distance, time):
-    speed = distance/time
-    return speed
+# import time
+# import math
+# def cal_speed(distance, time):
+#     speed = distance/time
+#     return speed
 
-is_speed = cal_speed(2, 60 * 10)
-print(is_speed)
+# is_speed = cal_speed(2, 60 * 10)
+# print(is_speed)
 
 
 # print(cal_speed)
 
-def greeting(name):
-    print(f"Hello {name}")
+# def greeting(name):
+#     print(f"Hello {name}")
 
-greeting('Olyvia')
+# greeting('Olyvia')
 
 
-# area of a circle
-def areaOfCircle(radius):
-    area = 22/7 * pow(radius,2)
-    # area = 22/7*radius**2
-    return area
+# # area of a circle
+# def areaOfCircle(radius):
+#     area = math.pi * radius * radius
+#     area = 22/7 * pow(radius,2)
+#     # area = 22/7*radius**2
+#     return area
 
-print(areaOfCircle(200))
+# print(areaOfCircle(200))
+
+
+
+# try:
+#     takenum = int(input("Enter a positive number: "))
+#     def is_even(takenum):
+
+#         if takenum % 2 == 0:
+#             print("true")
+#         elif takenum % 2 != 0:
+#             print("false")
+
+#     is_even(takenum)
+
+# except ValueError:
+#     print("Please try again")
+
+try:
+    def is_true(takenum):
+        result = takenum % 2 == 0
+        return result
+    print(is_true())
+
+except NameError:
+    print("sorry")
